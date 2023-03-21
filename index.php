52e117c894a0a144a5ddd6bac9bda43beced08e9
Abdul Wasi <abdul.wasi.al.afif@gmail.com>
2023-03-21 22:21:37 +0700

    Add Starter PHP HomePage

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./style/style.css" />
+    <title>Tugas PHP - HomePage</title>
+  </head>
+  <body>
+    <div class="main-content">
+      <h1>Tugas PHP</h1>
+      <section class="task">
+        <section class="card">
+          <p>
+            Buatlah form menggunakan PHP dan hitung rumus dari bangun datar
+            jajar genjang
+          </p>
+          <a href="#"><button>Demo Tugas PHP 1</button></a>
+        </section>
+      </section>
+    </div>
+  </body>
+</html>
