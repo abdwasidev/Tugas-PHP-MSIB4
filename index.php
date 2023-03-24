6a478e25388ed676a456172601a0ea7bf912614f
Abdul Wasi <abdul.wasi.al.afif@gmail.com>
2023-03-25 01:54:52 +0700

    Add Starter Tugas 2 PHP

--- index.php
+++ index.php
@@ -16,7 +16,40 @@
             Buatlah form menggunakan PHP dan hitung rumus dari bangun datar
             jajar genjang
           </p>
-          <a href="#"><button>Demo Tugas PHP 1</button></a>
+          <a href="/TugasPHP1"><button>Demo Tugas PHP 1</button></a>
+        </section>
+        <section class="card">
+          <p>
+            Diketahui sebuah perusahaan ingin memasukan rincian gaji ke pegawai dengan
+            menggunakan form proses PHP, dengan rincian sebagai berikut : <br /><br />
+            1. Tentukan gaji pokok (switch case)
+            <br />
+            Jika Manager = 20jt, Asisten = 15jt, Kabag = 10jt, Staff = 4jt
+            <br /><br />
+            2. Tentukan tunjangan jabatan = 20% dari gaji pokok
+            <br />
+            3. Tentukan tunjangan keluarga (if multi kondisi):
+            <br />
+            Jika sudah menikah dan anak maksimal 2 = 5% dari gapok
+            <br />
+            Jika sudah menikah dan anak antara 3 - 5 = 10% dari gapok
+            <br />
+            Selain itu belum dapat tunjangan keluarga
+            <br />
+            4. Tentukan gaji kotor
+            <br />
+            5. Tentukan zakat_profesi (ternary)
+            <br />
+            Jika ia muslim dan gaji kotor minimal 6 juta, ada zakat = 2.5% dari gaji kotor.
+            Selain itu tidak ada zakat
+            <br />
+            6. Tentukan take home pay
+            <br />
+
+            Masukan sourcode ke github dan copas link ke LMS<br />
+
+          </p>
+          <a href="/TugasPHP2"><button>Demo Tugas PHP 1</button></a>
         </section>
       </section>
     </div>
